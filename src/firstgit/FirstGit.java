5158fbca4ef9509fa24b84c64804c7b75e02eae1
JohnDeWalt <Jackie@10.30.12.25>
2017-08-29 15:41:03 -0500

    added another statement to main

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -12,6 +12,7 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("second comment");
     }
     
 }
